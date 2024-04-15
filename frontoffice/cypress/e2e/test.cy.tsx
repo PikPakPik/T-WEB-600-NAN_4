33917c1338dbb2d1ed55069db6f6ab656a6a2bf2
Alexandre.T <alexandre.tressel@icloud.com>
2024-04-15 15:37:55 +0200

    ci: test ci cypress

--- frontoffice/cypress/e2e/test.cy.tsx
+++ frontoffice/cypress/e2e/test.cy.tsx
@@ -2,10 +2,6 @@ describe('Just visit e2e test', () => {
     beforeEach(() => {
         cy.visit('/')
     })
-    
-    it('should visit', () => {
-        cy.url().should('eq', Cypress.config().baseUrl)
-    })
 
     it('should show counter', () => {
         cy.get('span').contains('0')
