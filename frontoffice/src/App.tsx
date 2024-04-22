794194c5e8181a8e4aa50bb8e97fc2a88a125bbe
Alexandre.T <alexandre.tressel@icloud.com>
2024-04-22 10:23:16 +0200

    add autoformat on save and extensions to install

--- frontoffice/src/App.tsx
+++ frontoffice/src/App.tsx
@@ -1,25 +1,25 @@
 // import { isAuthenticated } from "@/auth";
-import Homepage from "@/pages/home/index";
-import AboutPage from "@/pages/about";
-import TopBar from "@/common/Layout/TopBar";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Footer from "@/common/Layout/Footer";
+import Homepage from '@/pages/home/index'
+import AboutPage from '@/pages/about'
+import TopBar from '@/common/Layout/TopBar'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Footer from '@/common/Layout/Footer'
 
 // function ProtectedRoute({ children }: any) {
 // 	return isAuthenticated() ? children : <Navigate to='/sign-in' replace />;
 // }
 
 function App() {
-	return (
-		<BrowserRouter>
-			<TopBar />
-			<Routes>
-				<Route path='/' element={<Homepage />} />
-				<Route path='/about' element={<AboutPage />} />
-			</Routes>
-			<Footer />
-		</BrowserRouter>
-	);
+    return (
+        <BrowserRouter>
+            <TopBar />
+            <Routes>
+                <Route path="/" element={<Homepage />} />
+                <Route path="/about" element={<AboutPage />} />
+            </Routes>
+            <Footer />
+        </BrowserRouter>
+    )
 }
 
-export default App;
+export default App
