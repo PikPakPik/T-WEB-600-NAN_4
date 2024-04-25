64dbec06c565534dae2d3ad16bbe3324ad3e8b92
Alexandre.T <alexandre.tressel@icloud.com>
2024-04-25 18:52:29 +0200

    if product aleredy in cart and add again we add the quantity to the existing one

--- app/src/Controller/CartController.php
+++ app/src/Controller/CartController.php
@@ -5,11 +5,13 @@ namespace App\Controller;
 use App\DTO\CartDTO;
 use App\DTO\UpdateUserDTO;
 use App\Entity\OrderProduct;
+use App\Entity\Product;
 use App\Entity\User;
 use App\Repository\CartRepository;
 use App\Repository\ProductRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Attribute\MapRequestPayload;
 use Symfony\Component\Routing\Attribute\Route;
@@ -39,49 +41,71 @@ class CartController extends AbstractController
     #[Route('/{id}', name: 'app_add_product_to_cart', methods: ['POST'], format: 'json')]
     public function addProductToCart(
         #[MapRequestPayload] CartDTO $cartDTO,
-        ProductRepository $productRepository,
         CartRepository $cartRepository,
-        EntityManagerInterface $entityManager
+        EntityManagerInterface $entityManager,
+        Product $product
     ): Response {
         /** @var User $user */
         $user = $this->getUser();
 
-        $product = $productRepository->findOneBy(['id' => $cartDTO->getProductId()]);
-
         $cart = $cartRepository->findOneBy(['owner' => $user]);
-        $orderProduct = new OrderProduct();
-        $orderProduct->setProduct($product);
-        $orderProduct->setCart($cart);
-        $orderProduct->setQuantity($cartDTO->getQuantity());
-        $cart->addProduct($orderProduct);
 
-        $entityManager->persist($orderProduct);
-        $entityManager->flush();
+        $orderProduct = $cart->getProducts()->filter(function (OrderProduct $orderProduct) use ($product) {
+            return $orderProduct->getProduct()->getId() === $product->getId();
+        })->first();
 
-        return $this->json(
-            $cart,
-            context: [
-                'groups' => [
-                    'cart:read',
-                    'date:read',
+        if ($orderProduct) {
+            $orderProduct->setQuantity($orderProduct->getQuantity() + $cartDTO->getQuantity());
+
+            $entityManager->persist($orderProduct);
+            $entityManager->flush();
+
+            return $this->json(
+                $cart,
+                context: [
+                    'groups' => [
+                        'cart:read',
+                        'date:read',
+                    ]
                 ]
-            ]
-        );
+            );
+        } else {
+            $orderProduct = new OrderProduct();
+            $orderProduct->setProduct($product);
+            $orderProduct->setQuantity($cartDTO->getQuantity());
+            $orderProduct->setCart($cart);
+
+            $cart->addProduct($orderProduct);
+
+            $entityManager->persist($orderProduct);
+            $entityManager->flush();
+
+            return $this->json(
+                $cart,
+                context: [
+                    'groups' => [
+                        'cart:read',
+                        'date:read',
+                    ]
+                ]
+            );
+        }
     }
 
     #[Route('/{id}', name: 'app_update_cart', methods: ['PATCH'], format: 'json')]
     public function updateCart(
         #[MapRequestPayload] CartDTO $cartDTO,
         CartRepository $cartRepository,
-        EntityManagerInterface $entityManager
+        EntityManagerInterface $entityManager,
+        Product $product
     ): Response {
         /** @var User $user */
         $user = $this->getUser();
 
         $cart = $cartRepository->findOneBy(['owner' => $user]);
 
-        $orderProduct = $cart->getProducts()->filter(function (OrderProduct $orderProduct) use ($cartDTO) {
-            return $orderProduct->getProduct()->getId() === $cartDTO->getProductId();
+        $orderProduct = $cart->getProducts()->filter(function (OrderProduct $orderProduct) use ($product) {
+            return $orderProduct->getProduct()->getId() === $product->getId();
         })->first();
 
         $orderProduct->setQuantity($cartDTO->getQuantity());
--- app/src/DTO/CartDTO.php
+++ app/src/DTO/CartDTO.php
@@ -6,19 +6,9 @@ use Symfony\Component\Validator\Constraints as Assert;
 
 class CartDTO
 {
-    #[Assert\NotBlank]
-    public int $productId;
 
     #[Assert\NotBlank]
     public int $quantity;
-
-    /**
-     * @return int
-     */
-    public function getProductId(): int
-    {
-        return $this->productId;
-    }
 
     /**
      * @return int
