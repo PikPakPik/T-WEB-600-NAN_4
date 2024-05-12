2132843194c1afba40bcffde946ce5d3c7433d4a
Alexandre.T <alexandre.tressel@icloud.com>
2024-05-12 21:38:58 +0200

    cicd - fix: delete indent

--- frontoffice/.eslintrc.cjs
+++ frontoffice/.eslintrc.cjs
@@ -1,7 +1,6 @@
 module.exports = {
   root: true,
   env: { browser: true, es2020: true },
-  indent: [2, 2, { SwitchCase: 1}],
   extends: [
     'eslint:recommended',
     "plugin:@typescript-eslint/eslint-recommended",
